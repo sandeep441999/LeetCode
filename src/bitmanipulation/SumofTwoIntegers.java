926b0a47373821d6863b9c09a71e091629f471ab
Sandeep Pabbu <pabbusandeep143@gmail.com>
2026-06-04 10:27:27 -0500

    SumofTwoIntegers

---
+++ src/bitmanipulation/SumofTwoIntegers.java
@@ -0,0 +1,12 @@
+package bitmanipulation;
+
+public class SumofTwoIntegers {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
